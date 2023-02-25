f1b06b7f0e02b1b9ce9ee04d65368d119b5c3309
piyush1160 <piyushbhatia534@gmail.com>
2023-02-25 23:05:15 +0530

    code upload 4

---
+++ src/StackAndQueue/ValidParthensis.java
@@ -0,0 +1,33 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class ValidParthensis {
+    public boolean isValid(String s ){
+        Stack<Character> st  = new Stack<>();
+        for (char ch :s.toCharArray() )
+        {
+           if(ch =='(' || ch =='{' || ch=='['){
+               st.push(ch);
+           }else{
+               if(ch ==')' ){
+                   if(st.isEmpty() || st.pop()!='('){
+                       return false;
+                   }
+               }
+               if(ch =='}' ){
+                   if(st.isEmpty() || st.pop()!='}'){
+                       return false;
+                   }
+               }
+               if(ch ==']' ){
+                   if(st.isEmpty() || st.pop()!=']'){
+                       return false;
+                   }
+               }
+
+           }
+        }
+        return st.isEmpty();
+    }
+}
